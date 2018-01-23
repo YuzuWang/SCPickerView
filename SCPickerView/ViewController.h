4f26105c541f24cee5bb9ff81c7c554d0f1cee48
Mac <mac@MacdeMacBook-Pro.local>
2018-01-23 13:43:37 +0800

    Initial Commit

---
+++ SCPickerView/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SCPickerView
+//
+//  Created by Mac on 2018/1/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SCPickerView/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  SCPickerView
+//
+//  Created by Mac on 2018/1/23.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
